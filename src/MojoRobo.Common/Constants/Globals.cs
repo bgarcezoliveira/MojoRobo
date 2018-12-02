b4c4e2ff1c5466c78761a5c3f8fcf63bd49d52b7
Bruno Oliveira <bgarcez.oliveira@gmail.com>
2018-12-02 21:02:52 +0000

    Bug fix Orientation sequences

--- src/MojoRobo.Common/Constants/Globals.cs
+++ src/MojoRobo.Common/Constants/Globals.cs
@@ -9,7 +9,7 @@
         public const int GridLineWeight = 1;
         public const int DirectionsSequenceOrigin = 3;
         public static readonly char[] DirectionsSequenceNorth = { '3', '5', '8', 'N', 'E', 'S', 'W' };
-        public static readonly char[] DirectionsSequenceSouth = { '8', '1', '3', 'S', 'W', 'N', 'E' };
+        public static readonly char[] DirectionsSequenceSouth = { '3', '1', '8', 'S', 'E', 'N', 'W' };
         public static readonly char[] DirectionsSequenceEast = { 'S', '8', '1', 'E', 'S', 'W', 'N' };
         public static readonly char[] DirectionsSequenceWest = { '1', '3', '5', 'W', 'N', 'E', 'S' };
     }
